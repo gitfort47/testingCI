d6e37255cf8d0453da5aa897c8aef85760f7ee00
DanBrown47 <danwand47@gmail.com>
2021-04-22 11:28:55 +0530

    Another commit

--- src/main/java/input/data.java
+++ src/main/java/input/data.java
@@ -4,4 +4,5 @@ public class data {
 	public String facebook_url = "https://facebook.com/";
 	public String username="danwand47@gmail.com";
 	public String password="cz12cz12cz12";
+	public String test="test";
 }
